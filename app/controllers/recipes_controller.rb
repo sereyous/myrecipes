1a7d052920b2b19a92a1d565b35f881533c642dd
Justin Brown <sereyous@gmail.com>
2015-06-29 13:24:59 +0000

    create new and edit forms and partials

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,53 @@
+class RecipesController < ApplicationController 
+  def index 
+    @recipes = Recipe.all
+  end
+  
+  def show 
+    #binding.pry
+    @recipe = Recipe.find(params[:id]) 
+  end
+  
+  def new 
+    @recipe = Recipe.new
+  end
+  
+  def create 
+    # binding.pry
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find_by(id: 2)
+    
+    if ( @recipe.save )
+      flash[:success] = "Your recipe was created successfully"
+      redirect_to recipes_path
+    else 
+      render :new
+    end
+  end
+  
+  def edit 
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update 
+    @recipe = Recipe.find(params[:id])
+    
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was updated successfully"
+      redirect_to recipe_path(@recipe)
+    else 
+      render :edit
+    end
+    
+  end
+  
+  def destroy 
+    
+  end
+  
+  private 
+    def recipe_params
+      params.require(:recipe).permit(:name,:summary,:description)      
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get '/home', to: 'pages#home'
+  resources :recipes
 end
